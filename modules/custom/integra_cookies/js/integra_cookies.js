130f6345205632502bcb40c6c25c3601391b7fd0
sgargallo <sgargallo@gmail.com>
2024-02-08 09:25:12 +0100

    Actualización módulo de cookies para cumplir Consent Mode

--- modules/custom/integra_cookies/js/integra_cookies.js
+++ modules/custom/integra_cookies/js/integra_cookies.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function() {
 
 	//Verificamos si el usuario ya ha tomado una decisión sobre las cookies aceptadas
-    if (jQuery.cookie("aviso_cookies_revisado") != 1) {
+	if (jQuery.cookie("aviso_cookies_revisado") != 1) {
 
 		//No se ha mostrado el aviso de cookies hasta el momento o el usuario no ha tomado una decisión, mostramos el aviso de primer nivel
-        jQuery('#modalCookies').modal({ backdrop: 'static', keyboard: false });
-        jQuery('#modalCookies').modal('show');
+		jQuery('#modalCookies').modal({ backdrop: 'static', keyboard: false });
+		jQuery('#modalCookies').modal('show');
 
 		//Definimos los eventos de los botones que aparecen en la capa de primer nivel
-        jQuery('#aceptarTodasCookies').click(function () { guardarConsentimiento(tiposCookiesActivos); });
-        jQuery('#aceptarCookiesObligatorias').click(function () { guardarConsentimiento(tiposCookiesObligatorios); });
-    }
+		jQuery('#aceptarTodasCookies').click(function () { guardarConsentimiento(tiposCookiesActivos); });
+		jQuery('#aceptarCookiesObligatorias').click(function () { guardarConsentimiento(tiposCookiesObligatorios); });
+	}
 
 	//Evento del botón que despliega la capa de segundo nivel
 	jQuery('#verAjustesCookies').click(function () {
@@ -61,6 +61,9 @@ jQuery(document).ready(function() {
 			jQuery(this).find('.accordion-collapse.collapse').addClass('show');
 		}
 	});
+
+	//Sincronizamos los valores actuales con GTM al inicializar la página
+	sincronizarConsentimiento();
 });
 
 function guardarConsentimiento(tiposCookiesConsentidos) {
@@ -93,5 +96,54 @@ function guardarConsentimiento(tiposCookiesConsentidos) {
 
   //Ocultamos la modal
   jQuery('#modalCookies').modal('hide');
+
+  //Sincronizamos valores con GTM
+  sincronizarConsentimiento();
 }
 
+function sincronizarConsentimiento() {
+
+  //Creamos un array con los tipos de consentimiento y su estado por defecto
+  var consentimientos = {};
+
+  //Obtenemos los tipos de consentimiento habilitados
+  var tiposConsentimiento = tiposConsentimientoActivos.split(",");
+
+  //Repasamos todos los tipos de consentimiento habilitados y establecemos su valor más restrictivo por defecto
+  tiposConsentimiento.forEach(function(valor, indice, array) { consentimientos[valor] = "denied"; });
+
+  //Obtenemos los tipos de cookies habilitados
+  var tiposCookies = tiposCookiesActivos.split(",");
+
+  //Repasamos todos los tipos de cookies habilitados
+  tiposCookies.forEach(function(valor, indice, array) {
+
+	//Comprobamos si el tipo de cookie actual está entre las habilitadas por el usuario
+	if (jQuery.cookie("acepta_cookies_" + valor) == 1) {
+
+	  //Extraemos los consentimientos asociados a la misma
+	  tiposConsentimientoHabilitar = (jQuery("#cookies_" + valor).attr('tipos_consentimiento')).split(",");
+
+	  //Repasamos todos los tipos de consentimieto que aplican en esta cookie
+	  tiposConsentimientoHabilitar.forEach(function(valor, indice, array) {
+		if (valor != "") {
+		  consentimientos[valor] = "granted";
+		}
+	  });
+	}
+  });
+
+  //console.log(consentimientos);
+
+  //Revisamos si ya ha aceptado el aviso de cookies o no para generar el default o un update
+  if (jQuery.cookie("aviso_cookies_revisado") != 1) {
+
+	//Primer acceso a la web sin operar previamente con el aviso
+	if (typeof gtag !== 'undefined') { gtag('consent', 'default', consentimientos); }
+
+  } else {
+
+	//Recarga de la página con aviso ya consultado o visita posterior con preferencias cargadas en las cookies
+	if (typeof gtag !== 'undefined') { gtag('consent', 'update', consentimientos); }
+  }
+}
